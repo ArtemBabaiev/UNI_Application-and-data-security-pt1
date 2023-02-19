2c0ea8367a65c7d7340595076947557cef7a6a55
ArtemBabaiev <90097902+ArtemBabaiev@users.noreply.github.com>
2023-02-19 21:30:19 +0200

    lab2 initial commit

---
+++ AffineCipher/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AffineCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
