1dd26ee9826e0a3780e1db94710af692ecdfbf08
ArtemBabaiev <90097902+ArtemBabaiev@users.noreply.github.com>
2023-03-23 13:32:17 +0200

    Lab 7 complete

---
+++ Diffie-Hellman Algorithm/DiffieHellmanAlgorithm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diffie_Hellman_Algorithm
+{
+    internal class DiffieHellmanAlgorithm
+    {
+        long a;
+        long p;
+        public long PrivateKey { get; set; }
+
+        public DiffieHellmanAlgorithm(long a, long p)
+        {
+            this.a = a;
+            this.p = p;
+            PrivateKey = new Random().Next(1, 10);
+        }
+
+        public long GenerateKey()
+        {
+            return PowerModule(a, PrivateKey, p);
+        }
+
+        public long GetSecretKey(long x)
+        {
+            return PowerModule(x, PrivateKey, p);
+        }
+
+        private long PowerModule(long number, long power, long mod)
+        {
+            if (power == 1)
+                return number;
+            var powered = Math.Pow(number, power);
+            return (long)powered % mod;
+        }
+    }
+}
---
+++ Diffie-Hellman Algorithm/Program.cs
@@ -0,0 +1,37 @@
+﻿using ClassLibrary;
+
+namespace Diffie_Hellman_Algorithm
+{
+    internal class Program
+    {
+       static void Main(string[] args)
+        {
+            var primeNumbers = MathHelper.SieveOfEratosthenes(2000);
+            long p = primeNumbers.Last();
+            long a = MathHelper.FindPrimitive(p);
+            Console.WriteLine($"Public keys: p={p}; a={a}");
+            var alg1 = new DiffieHellmanAlgorithm(a, p);
+            var alg2 = new DiffieHellmanAlgorithm(a, p);
+
+            var pr1 = alg1.GenerateKey();
+            Console.WriteLine($"First generated key {pr1}");
+            var pr2 = alg2.GenerateKey();
+            Console.WriteLine($"Second generated key {pr2}");
+
+            var key1 = alg1.GetSecretKey(pr2);
+            var key2 = alg2.GetSecretKey(pr1);
+            Console.WriteLine($"{key1} -> {key2}");
+        }
+
+        private static long power(long a, long b, long P)
+        {
+            if (b == 1)
+                return a;
+
+            else
+                return (((long)Math.Pow(a, b)) % P);
+        }
+        
+
+    }
+}
